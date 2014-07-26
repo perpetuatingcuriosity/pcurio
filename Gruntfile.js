2155439fcbb7df48f77a09746d52cb65fc8ea388
perpetuatingcuriosity <perpetuatingcuriosity@gmail.com>
2014-07-26 00:20:45 -0400

    grunt update: starter, watch, production tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,16 @@ module.exports = function(grunt) {
 // Project configuration.
 grunt.initConfig({
 	pkg: grunt.file.readJSON('package.json'),
+	
+	//MADE DIRECTORY
+	mkdir: {
+	    all: {
+	      options: {
+	        create: ['production', 'production/css','production/js']
+	      }
+	    }
+	  },
+
 	//SASS
 	sass: {
 		dist:{
@@ -14,8 +24,8 @@ grunt.initConfig({
 
 	//JADE
 	jade: {
-		compile : {
-			options : {
+		compile: {
+			options: {
 				data: {
 					debug: false
 				}
@@ -35,6 +45,7 @@ grunt.initConfig({
 			src: 'css/style.css'
 		}
 	},
+
 	//CONNECT
 	connect: {
 		server: {
@@ -44,6 +55,60 @@ grunt.initConfig({
 			}
 		}
 	},
+
+	//SVG INJECT
+	svginject: {
+	   all : {
+	     options: {},
+	     files: {
+	        'js/SVGinject.js': ['images/*.svg'],
+	     }
+	   }
+	 },
+
+	//CONCATENATE FILES
+	concat: {
+		options: {
+			separator: ';'
+		},
+	  css: {
+	    src: ['css/*.css'],
+	    dest: 'production/css/style.css'  
+	  },
+	  js: {
+	    src: ['js/*.js'],
+	    dest: 'production/js/main.js'   
+	   }
+	 },
+
+	//CSS MINIFY
+	cssmin: {
+	  minify: {
+	    expand: true,
+	    cwd: 'production/css',
+	    src: ['*.css', '!*.min.css'],
+	    dest: 'production/css/',
+	    ext: '.min.css'
+	  }
+	},
+
+	//JS UGLIFY
+	uglify: {
+	    my_target: {
+	      files: {
+	        'production/js/main.min.js': ['production/js/*.js']
+	      }
+	    }
+	  },
+
+	//COPY
+	copy: {
+	  main: {
+	    src: 'index.html',
+	    dest: 'production/',
+	  }
+	},	  
+
 	//WATCH
 	watch: {
 		css: {
@@ -61,13 +126,34 @@ grunt.initConfig({
 
 }); //end
 
+//STARTER
+grunt.loadNpmTasks('grunt-mkdir');
+
+//WATCH
 grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-autoprefixer');
-grunt.loadNpmTasks('grunt-contrib-connect');
 grunt.loadNpmTasks('grunt-contrib-jade');
+grunt.loadNpmTasks('grunt-autoprefixer');
+grunt.loadNpmTasks('grunt-svginject');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+//PRODUCTION
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-copy');
+
 
 // Default task(s).
-grunt.registerTask('default', ['watch']);
+grunt.registerTask('default', ['connect', 'watch']);
+grunt.registerTask('svg', ['svginject']);
+grunt.registerTask('starter',['mkdir']);
+grunt.registerTask('mkprod',['concat','cssmin','uglify','copy']);
 
 };
+
+
+
+
+
+
